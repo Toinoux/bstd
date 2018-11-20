b19a6fc01229279ea55af4d5090c53bbe392f947
the-courthouse <bguilliod@gmail.com>
2018-11-20 18:08:14 +0100

    [ADD] refactor tcp socket

--- network/TCPSocket.hpp
+++ network/TCPSocket.hpp
@@ -22,8 +22,16 @@
 namespace bstd::network {
 	class TCPSocket : public Socket {
 	public:
-		TCPSocket() : Socket(SOCK_STREAM) {
-		};
+		TCPSocket(size_t CONNECTION_MAX = 1) : Socket(SOCK_STREAM), _port(bind()) {
+			listen(CONNECTION_MAX);
+		}
+
+		TCPSocket(PORT port, const std::string &host = DEFAULT_HOST) : Socket(SOCK_STREAM) {
+			if (port != 0) {
+				connect(port, host);
+				std::cout << "Connected to " << host << ":" << port << std::endl;
+			}
+		}
 
 	public:
 		void send(SOCKET sock, std::string_view buffer, size_t len, int flags = 0) const {
@@ -46,7 +54,15 @@ namespace bstd::network {
 			}
 			return str;
 		};
+
+		PORT getPort() const {
+			return (_port);
+		}
+
 		virtual ~TCPSocket() {};
+
+	private:
+		PORT _port;
 
 	private:
 		static inline const int READ_SIZE = 4096;
@@ -54,10 +70,9 @@ namespace bstd::network {
 
 	class TCPBasicEchoServer : public TCPSocket {
 	public:
-		TCPBasicEchoServer(size_t CONNECTION_MAX = 1, bool verbose = true) : TCPSocket(), _port(bind()) {
-			listen(CONNECTION_MAX);
+		TCPBasicEchoServer(size_t CONNECTION_MAX = 1, bool verbose = true) : TCPSocket(CONNECTION_MAX) {
 			if(verbose)
-				std::cout << "Listening on port " << _port << ":" << std::endl;
+				std::cout << "Listening on port " << getPort() << ":" << std::endl;
 		};
 
 		void run() {
@@ -66,7 +81,7 @@ namespace bstd::network {
 			while (1) {
 				SOCKET csock;
 
-				csock = this->accept(_port);
+				csock = this->accept(getPort());
 				while(1) {
 					msg = this->revc(csock);
 					std::cout << msg << std::endl;
@@ -79,15 +94,11 @@ namespace bstd::network {
 		}
 
 		~TCPBasicEchoServer() {};
-	private:
-		PORT _port;
 	};
 
 	class TCPBasicEchoClient : public TCPSocket {
 	public:
-		TCPBasicEchoClient(PORT port, const std::string &host = DEFAULT_HOST) : TCPSocket() {
-			connect(port, host);
-			std::cout << "Connected to " << host << ":" << port << std::endl;
+		TCPBasicEchoClient(PORT port, const std::string &host = DEFAULT_HOST) : TCPSocket(port, host) {
 		};
 
 		void run() {
